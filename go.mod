f764861727b217a0b2a0710aeb998c87adbcd2a4
riku_6174 <riku.saso.822@gmail.com>
2024-07-15 20:54:22 +0900

    handlerパッケージを作成し、コードを分割した

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22.1
 require (
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.12.0
 )
 
--- main.go
+++ main.go
@@ -1,26 +1,23 @@
 package main
 
 import (
-	"database/sql"
-	"errors"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"time"
+
 	"github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/traPtitech/naro-template-backend/handler"
 )
-type City struct {
-	ID          int    `json:"id,omitempty"  db:"ID"`
-	Name        string `json:"name,omitempty"  db:"Name"`
-	CountryCode string `json:"countryCode,omitempty"  db:"CountryCode"`
-	District    string `json:"district,omitempty"  db:"District"`
-	Population  int    `json:"population,omitempty"  db:"Population"`
-}
-var db *sqlx.DB
+
 func main(){
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	jst, err := time.LoadLocation("Asia/Tokyo")
 	if err != nil {
 		log.Fatal(err)
@@ -35,43 +32,19 @@ func main(){
 		Collation: "utf8mb4_unicode_ci",
 		Loc:       jst,
 	}
-	_db, err := sqlx.Open("mysql", conf.FormatDSN())
+
+	db, err := sqlx.Open("mysql", conf.FormatDSN())
 	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("connected")
-	db = _db
+	
+	h := handler.NewHandler(db)
 	e := echo.New()
-	e.GET("/cities/:cityName", getCityInfoHandler)
-	e.POST("/cities", addCityHandler)
-	e.Logger.Fatal(e.Start(":8080"))
-}
-func getCityInfoHandler(c echo.Context) error {
-	cityName := c.Param("cityName")
-	log.Println(cityName)
-	var  city City
-	err := db.Get(&city,"SELECT * FROM city WHERE Name = ?",cityName)
-	if errors.Is(err,sql.ErrNoRows){
-		return echo.NewHTTPError(http.StatusNotFound,fmt.Sprintf("No such city Name = %s",cityName))
-	}
+	e.GET("/cities/:cityName", h.GetCityInfoHandler)
+	e.POST("/cities", h.PostCityHandler)
+	
+	err = e.Start(":8080")
 	if err != nil {
-		log.Printf("DB Error: %s", err)
-		return echo.NewHTTPError(http.StatusInternalServerError, "internal server error")
+		log.Fatal(err)
 	}
-	return c.JSON(http.StatusOK, city)
-}
-func addCityHandler(c echo.Context) error {
-	city := City{}
-	err := c.Bind(&city)
-	if err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest,fmt.Sprintf("%+v",err))
-	}
-	result,err := db.Exec("INSERT INTO city (Name, CountryCode, District, Population) VALUES (?,?,?,?)",city.Name,city.CountryCode,city.District,city.Population)
-	if err != nil {
-		log.Printf("DB Error:%s",err)
-		return echo.NewHTTPError(http.StatusBadRequest,"internal server error")
-	}
-	id ,_ := result.LastInsertId()
-	city.ID = int(id)
-	return c.JSON(http.StatusOK,city)
 }
